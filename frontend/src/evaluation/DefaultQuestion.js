2ba12996e4488b79b6b17d6600a752a270ddec80
Alex Rody <hey@alexrody.com>
2022-04-25 01:37:38 -0500

    Add formatting to titles, questions and instructions

--- frontend/src/evaluation/DefaultQuestion.js
+++ frontend/src/evaluation/DefaultQuestion.js
@@ -5,8 +5,8 @@ export default function DefaultQuestion({question, qNum, scores}) {
     return (
         <>
             <div className="question">
-                <p className="instructions">{question.instructions}</p>
-                <h1 className="question">{question.text}</h1>
+                <p className="instructions">{question.note && <><b>{question.note}</b><br /></>} {question.instructions}</p>
+                <h1 className="question-text">{question.text}</h1>
             </div>
             <div id="buttons" className="score-buttons">
                 {question.scores.map(x => <div><button key={`${x}`} onClick={() => scores[qNum]=x}>{x}</button></div>)}

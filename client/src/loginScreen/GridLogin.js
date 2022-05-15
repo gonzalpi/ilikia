fe04f4fbfa04090ed43d8c0f77f18a51ff916a72
Alex Rody <hey@alexrody.com>
2022-05-15 01:11:49 -0500

    Move GridLogin style to CSS file

--- client/src/loginScreen/GridLogin.js
+++ client/src/loginScreen/GridLogin.js
@@ -1,9 +1,6 @@
 import React, { Component, useState, useEffect } from 'react';
 import './GridLogin.css';
-import {GridInputBox} from './GridInputBox';
-import {WelcomeText} from './WelcomeText';
 import {useNavigate} from "react-router-dom";
-import logo from './ilikia.svg';
 
 function GridLogin() {
 
@@ -56,95 +53,20 @@ function GridLogin() {
 
         <div className='GridLogin'>
             
+            <h1 className='gl-title'>Ilikia</h1>
 
-            <h1 style={{
+            <p className='gl-text'>Usuario</p>
+            <input className='gl-input' value={input} onInput= {e => setInput(e.target.value)}/>
 
-                fontSize: "4em",
-                fontFamily: "Montserrat",
-                color: "#556500",
-                margin: "0"
+            <p className='gl-text'>Contraseña</p>
+            <input className='gl-input' style={{marginBottom: "3%"}} type="password"/>
 
-
-            }}>Ilikia</h1>
-            
-            <p style={{
-
-                fontFamily: 'Montserrat',
-                fontWeight: "700",
-                fontSize: "2em",
-
-            }}>Usuario</p>
-            
-            <input style={{
-
-                textAlign: "left",
-                position: "sticky",
-                color: "rgb(25, 29, 8, 0.8)",
-                fontFamily: "Montserrat",
-                fontWeight: "700",
-                fontSize: "2em",
-                borderRadius: "40px",
-                padding: "4%",
-                backgroundColor: "#E1E5C3",
-                border: 'none'
-
-            }} value={input} onInput= {e => setInput(e.target.value)}/>
-
-            <p style={{
-
-                fontFamily: 'Montserrat',
-                fontWeight: "700",
-                fontSize: "2em",
-
-            }}>Contraseña</p>
-
-            <input style={{
-
-                textAlign: "left",
-                position: "sticky",
-                color: "rgb(25, 29, 8, 0.8)",
-                fontFamily: "Montserrat",
-                fontWeight: "700",
-                fontSize: "2em",
-                borderRadius: "40px",
-                padding: "4%",
-                backgroundColor: "#E1E5C3",
-                border: 'none',
-                marginBottom: "3%"
-
-            }} type="password"/>
-
-
-            <button style={{
-
-                backgroundColor: "#D7EC7C",
-                width: "50%",
-                height: "4.5em", 
-                margin: "auto",
-                // marginTop: "8%",
-                borderRadius: "40px",
-                fontFamily: 'Montserrat',
-                fontWeight: '700',
-                fontSize: '1.3em',
-                textAlign: 'center',
-                border: "none",
-                color: "#3D4719",
-                cursor: "pointer"
-
-            }} onClick= { () => {
-                
-                console.log(user, personal, input, medico)
-                if (user == 0) {
-
-                    navigate("/minimental/" + personal + "/" + input + "/" + medico + "/1")
-
-                } else if (user == 1) {
-
-                    navigate("/menu")
-                
-                }
-
-
+            <button className='gl-button'
+            onClick={() => {
+                if (user == 0)
+                    navigate("/minimental/" + personal + "/" + input + "/" + medico + "/1");
+                else if (user == 1)
+                    navigate("/menu");
             }}> Continuar </button>
 
         </div>

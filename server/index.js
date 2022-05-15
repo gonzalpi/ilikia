7b2c71bbf22ca8cf9bca7920bccfaacac8653cdb
Alex Rody <hey@alexrody.com>
2022-05-15 00:42:09 -0500

    Endpoints: fix usertype, add staff, and document the rest
    Fix /api/usertype endpoint to handle requests of non-existent users. Add /api/staff endpoint that returns users of all personnel in charge of a given patient. Comment the output of all endpoints.

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const con = mysql.createConnection({
     user: "root",
     password: "root",
     database: "ilikia",
-    // socketPath: "/var/lib/mysql/mysql.sock"
+    socketPath: "/var/lib/mysql/mysql.sock"
 });
 
 con.connect(function(err) {
@@ -29,6 +29,12 @@ localhost:3001/api/exam
     personal=aaa
     paciente=bbb
     medico=ccc
+// RETORNO
+    [
+        {id_examen, usuario_personal, usuario_paciente, usuario_medico, tipo, fecha,
+        total, cat_1, ..., cat_10, nombre_paciente, nombre_personal, nombre_medico},
+        ...
+    ]
 */
 app.get("/api/exam", (req, res) =>
 {
@@ -66,6 +72,8 @@ app.get("/api/exam", (req, res) =>
 localhost:3001/api/name
 // PARÁMETROS COMPULSORIOS
     usuario=aaa
+// RETORNO
+    [{nombre}]
 */
 app.get("/api/name", (req, res) =>
 {
@@ -141,30 +149,78 @@ app.post("/api/exam", (req, res) =>
 });
 
 /*
-localhost:3001/api/exam
+localhost:3001/api/usertype
 // PARÁMETROS COMPULSORIOS
     usuario=aaa
-// NOTA: usuario debe existir, si no, no retorna nada aún
+// RETORNO
+    {tipo}
+    -1 : usuario inexistente
+     0 : paciente
+     1 : doctor responsable
+     2 : personal de salud
 */
 app.get("/api/usertype", (req, res) =>
 {
     if (req.query.usuario)
     {
-        // setTimeout(() => {return res.send({"tipo": -1});}, 2000)
         con.query(
-            `SELECT usuario FROM paciente WHERE paciente.usuario='${req.query.usuario}';`,
+            `SELECT usuario FROM usuario WHERE usuario.usuario='${req.query.usuario}';`,
             (err, results, fields) =>
             {
-                if (err) {res.send(err);}
-                else if (results.length > 0) {res.send({"tipo": 0});}
+                if (results.length > 0)
+                {
+                    con.query(
+                        `SELECT usuario FROM paciente WHERE paciente.usuario='${req.query.usuario}';`,
+                        (err, results, fields) =>
+                        {
+                            if (err) {res.send(err);}
+                            else if (results.length > 0) {res.send({"tipo": 0});}
+                        }
+                    );
+                    con.query(
+                        `SELECT rol FROM personal_salud WHERE personal_salud.usuario='${req.query.usuario}';`,
+                        (err, results, fields) =>
+                        {
+                            if (err) {res.send(err);}
+                            else if (results.length > 0) {res.send({"tipo": results[0].rol});}
+                        }
+                    );
+                }
+                else
+                {
+                    res.send({"tipo": -1});
+                }
             }
-        );
+        )
+    }
+    else
+    {
+        res.status(400);
+        res.send("Error: Solicitud incompleta");
+    }
+});
+
+/*
+localhost:3001/api/staff
+// PARÁMETROS COMPULSORIOS
+    usuario=aaa
+// RETORNO
+    [
+        {usuario_paciente},
+        ...
+    ]
+*/
+app.get("/api/staff", (req, res) =>
+{
+    if (req.query.usuario)
+    {
         con.query(
-            `SELECT rol FROM personal_salud WHERE personal_salud.usuario='${req.query.usuario}';`,
+            `SELECT usuario_personal FROM atiende WHERE atiende.usuario_paciente='${req.query.usuario}';`,
             (err, results, fields) =>
             {
-                if (err) {res.send(err);}
-                else if (results.length > 0) {console.log(res); res.send({"tipo": results[0].rol});}
+                err ?
+                res.send(err) :
+                res.send(results)
             }
         );
     }
